7c1a368490918cd415d2447b2967bc9cc208ca71
Alexander <allatsnow@gmail.com>
2020-11-20 20:47:56 +0300

    меняет кавычки на однообразные

--- src/main.js
+++ src/main.js
@@ -13,29 +13,29 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteHeaderElement = document.querySelector(`.header`);
-const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = document.querySelector(".header");
+const siteMainElement = document.querySelector(".main");
 const siteBody = document.querySelector("body");
 
 
-render(siteHeaderElement, createHeaderProfileTemplate(), `beforeend`);
-render(siteMainElement, createMainMenuTemplate(), `beforeend`);
-render(siteMainElement, createMainListSortTemplate(), `beforeend`);
-render(siteMainElement, createMainStatisticTemplate(), `beforeend`);
-render(siteMainElement, createMainFilmContainerTemplate(), `beforeend`);
+render(siteHeaderElement, createHeaderProfileTemplate(), "beforeend");
+render(siteMainElement, createMainMenuTemplate(), "beforeend");
+render(siteMainElement, createMainListSortTemplate(), "beforeend");
+render(siteMainElement, createMainStatisticTemplate(), "beforeend");
+render(siteMainElement, createMainFilmContainerTemplate(), "beforeend");
 
-const mainFilmListContainer = document.querySelector('.films-list__container--main');
-const ratedFilmListContainer = document.querySelector('.films-list__container--rated');
-const commentedFilmListContainer = document.querySelector('.films-list__container--commented');
+const mainFilmListContainer = document.querySelector(".films-list__container--main");
+const ratedFilmListContainer = document.querySelector(".films-list__container--rated");
+const commentedFilmListContainer = document.querySelector(".films-list__container--commented");
 
 for (let i = 0; i < MAIN_TASK_COUNT; i++) {
-  render(mainFilmListContainer, createFilmCardTemplate(), `beforeend`);
+  render(mainFilmListContainer, createFilmCardTemplate(), "beforeend");
 }
 
 for (let i = 0; i < SECONDARY_TASK_COUNT; i++) {
-  render(ratedFilmListContainer, createFilmCardTemplate(), `beforeend`);
-  render(commentedFilmListContainer, createFilmCardTemplate(), `beforeend`);
+  render(ratedFilmListContainer, createFilmCardTemplate(), "beforeend");
+  render(commentedFilmListContainer, createFilmCardTemplate(), "beforeend");
 }
 
-render(siteBody, createFilmDetailPopupTemplate(), `beforeend`);
+render(siteBody, createFilmDetailPopupTemplate(), "beforeend");
 
